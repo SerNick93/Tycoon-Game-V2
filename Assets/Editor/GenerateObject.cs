0ce8348cb881013e645d3de6c1bef7154ed0d32d
SerNick93 <njones0091@gmail.com>
2020-06-05 23:34:56 +0100

    5-6-20

---
+++ Assets/Editor/GenerateObject.cs
@@ -0,0 +1,202 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class GenerateObject : EditorWindow
+{
+    string objectName;
+    string path;
+
+    Object meshObject;
+    GameObject newMesh;
+
+
+    [Tooltip("Not currently Working")]
+    LayerMask layerMask;
+    static int defaultLayer = 0;
+    static int flags = 0;
+    static string[] options = new string[] { "Null0", "Null1", "Null2", "Null3", "Null4", "Null5", "Null6", "Null7", "Ramps"};
+    bool placeUnderground = false;
+    GameObject go;
+    GameObject snapPoints;
+    private void OnEnable()
+    {
+        path = "Assets/Resources/Prefabs";
+        
+    }
+
+    [MenuItem("Generators/Generate Object")]
+    public static void ShowWindow()
+    {
+        GetWindow<GenerateObject>("Generate Object");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Generate an object to use in game.", EditorStyles.boldLabel);
+        objectName = EditorGUILayout.TextField("Object Name", objectName);
+        
+        //Not working
+        //defaultLayer = EditorGUILayout.LayerField("Collision Ignore Layer", defaultLayer);
+
+        //Use this for layermasks
+        flags = EditorGUILayout.MaskField("Flags", flags, options);
+        placeUnderground = EditorGUILayout.Toggle("Underground Object", placeUnderground);
+        meshObject = EditorGUILayout.ObjectField("New Object Model", meshObject, typeof(Object), false);
+
+        if (GUILayout.Button("Generate Game Object"))
+        {
+            if (objectName != "")
+            {
+                GenerateDirectory((objectName));
+            }
+            else
+                Debug.Log("Please specify an object name");
+        }
+    }
+
+
+    void GenerateDirectory(string objectName)
+    {
+        string fullPath = Path.Combine(path + "/", objectName);
+
+        Directory.CreateDirectory(fullPath);
+        GenerateGameObject(fullPath);
+    }
+
+    void GenerateGameObject(string path)
+    {
+        go = new GameObject(objectName);
+        Transform t = go.GetComponent<Transform>();
+        GameObject newMesh = (GameObject)Instantiate(meshObject, t);
+        newMesh.transform.name = objectName + "Child";
+
+        newMesh.transform.SetParent(t);
+
+        go.AddComponent<BoxCollider>();
+        go.AddComponent<PlaceObjectInWorld>();
+        go.AddComponent<EditModel>().enabled = false;
+
+        ScriptableObjectUtility.CreateAsset<SO_Object>(path, objectName);
+        MoveToPivot(go);
+
+    }
+    void MoveToPivot(GameObject go)
+    {
+        Transform tChild = go.transform.GetChild(0).GetComponent<Transform>();
+        tChild.position = new Vector3(tChild.localScale.x / 2, tChild.position.y, tChild.localScale.z / 2);
+        //x position is localscale / 2
+        //y position is the same
+        //z position is local scale / 2
+        SetupDependencies(go);
+
+    }
+
+    void SetupDependencies(GameObject go)
+    {
+        PlaceObjectInWorld p = go.GetComponent<PlaceObjectInWorld>();
+        //Box Collider
+        BoxCollider bc = go.GetComponent<BoxCollider>();
+        p.ObjectBoxCollider = bc;
+        Transform tChild = go.transform.GetChild(0).GetComponent<Transform>();
+        MeshRenderer mr = tChild.GetComponent<MeshRenderer>();
+        go.layer = 8;
+        tChild.gameObject.layer = 8;
+
+        bc.center = mr.bounds.center;
+        bc.size = mr.bounds.size - new Vector3(0.1f, -0.1f, 0.1f);
+
+        //Child's Transform
+        p.ObjectTransform = tChild;
+        //Child's MeshRenderer
+        p.ObjectMeshRenderer = tChild.gameObject.GetComponent<MeshRenderer>();
+        //Setting the collision flags
+        p.LayerMask = flags;
+        //Reference the material manager
+        p.MaterialManager = MaterialManager.MyInstance;
+        SetupScriptableObject(go);
+    }
+    void SetupScriptableObject(GameObject go)
+    {
+        PlaceObjectInWorld p = go.GetComponent<PlaceObjectInWorld>();
+        SO_Object so = Resources.Load<SO_Object>("Prefabs/" + objectName + "/" + objectName) as SO_Object;
+        Debug.Log                 ("Prefabs/" + objectName + "/" + objectName);
+        Debug.Log(so);
+        p.PlaceableObject = so;
+        SetupSnapPoints(go);
+    }
+    void SetupSnapPoints(GameObject go)
+    {
+        Transform tChild = go.transform.GetChild(0).GetComponent<Transform>();
+        Vector3Int[] localCoordinates = new Vector3Int[4];
+        //localCoordinates[0] = new Vector3Int(0, 0, 0);
+        //localCoordinates[1] = new Vector3Int(1, 0, 0);
+        //localCoordinates[2] = new Vector3Int(0, 0, 1);
+        //localCoordinates[3] = new Vector3Int(1, 0, 1);
+        //localCoordinates[4] = new Vector3Int(0, 1, 0);
+        //localCoordinates[5] = new Vector3Int(1, 1, 0);
+        //localCoordinates[6] = new Vector3Int(0, 1, 1);
+        //localCoordinates[7] = new Vector3Int(1, 1, 1);
+
+        localCoordinates[0] = new Vector3Int(
+            (int)tChild.localPosition.x,
+            (int)tChild.localPosition.y,
+            (int)tChild.localPosition.z
+            );
+
+        localCoordinates[1] = new Vector3Int(
+            (int)tChild.localPosition.x - (int)tChild.localScale.x,
+            (int)tChild.localPosition.y,
+            (int)tChild.localPosition.z
+            );
+        localCoordinates[2] = new Vector3Int(
+            (int)tChild.localPosition.x - (int)tChild.localScale.x,
+            (int)tChild.localPosition.y,
+            (int)tChild.localPosition.z - (int)tChild.localScale.z
+            );
+        localCoordinates[3] = new Vector3Int(
+            (int)tChild.localPosition.x,
+            (int)tChild.localPosition.y,
+            (int)tChild.localPosition.z - (int)tChild.localScale.z
+            );
+
+        if (!placeUnderground)
+        {
+            localCoordinates = new Vector3Int[8];
+            localCoordinates[4] = new Vector3Int(
+                (int)tChild.localPosition.x,
+                (int)tChild.localPosition.y,
+                (int)tChild.localPosition.z
+                );
+
+            localCoordinates[5] = new Vector3Int(
+                (int)tChild.localPosition.x - (int)tChild.localScale.x,
+                (int)tChild.localPosition.y,
+                (int)tChild.localPosition.z
+                );
+            localCoordinates[6] = new Vector3Int(
+                (int)tChild.localPosition.x - (int)tChild.localScale.x,
+                (int)tChild.localPosition.y,
+                (int)tChild.localPosition.z - (int)tChild.localScale.z
+                );
+            localCoordinates[7] = new Vector3Int(
+                (int)tChild.localPosition.x,
+                (int)tChild.localPosition.y,
+                (int)tChild.localPosition.z - (int)tChild.localScale.z
+                );
+
+        }
+
+
+        snapPoints = new GameObject("SnapPoint");
+        snapPoints.AddComponent<SphereCollider>().radius = 0.05f;
+        for (int i = 0; i < localCoordinates.Length; i++)
+        {
+            Instantiate(snapPoints, localCoordinates[i], Quaternion.identity, tChild);
+        }
+
+    }
+    //Add the snapping points
+    //Create a prefab of the new object, then delete it from the scene.
+
+}
---
+++ Assets/Resources/Scripts/Model Editing Tools/PlaceObjectInWorld.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+using Nick.controllers;
+
+public class PlaceObjectInWorld : MonoBehaviour
+{
+    Vector3 pos, newPos;
+    static int numOfRamps = 0;
+    bool colliderAlert = false;
+    
+    [SerializeField]
+    BoxCollider objectBoxCollider;
+    [SerializeField]
+    Transform objectTransform;
+    [SerializeField]
+    MeshRenderer objectMeshRenderer;
+    
+    [SerializeField] MaterialManager materialManager;
+    
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] SO_Object placeableObject;
+    public SO_Object PlaceableObject { get => placeableObject; set => placeableObject = value; }
+    public bool ColliderAlert { get => colliderAlert; set => colliderAlert = value; }
+    public MeshRenderer ObjectMeshRenderer { get => objectMeshRenderer; set => objectMeshRenderer = value; }
+    public BoxCollider ObjectBoxCollider { get => objectBoxCollider; set => objectBoxCollider = value; }
+    public Transform ObjectTransform { get => objectTransform; set => objectTransform = value; }
+    public LayerMask LayerMask { get => layerMask; set => layerMask = value; }
+    public MaterialManager MaterialManager { get => materialManager; set => materialManager = value; }
+
+    public void InitScripts()
+    {
+        ObjectBoxCollider = GetComponent<BoxCollider>();
+        ObjectTransform = transform.GetChild(0).GetComponent<Transform>();
+        ObjectMeshRenderer = ObjectTransform.GetComponent<MeshRenderer>();        
+
+        //cc.isTrigger = true;
+        ObjectBoxCollider.center = ObjectMeshRenderer.bounds.center;
+        ObjectBoxCollider.size = ObjectMeshRenderer.bounds.size - new Vector3(0.1f, -0.1f, 0.1f);
+        
+        gameObject.layer = 8;
+
+        MaterialManager = MaterialManager.MyInstance;
+
+        SetRampActive();
+    }
+
+    void FixedUpdate()
+    {
+        
+        //Move model's posision with mouse movment.
+        if (TransformControls.MyInstance.SnapToGridToggle.isOn)
+        {
+            /*If locked to grid.*/
+            newPos = CustomControllers.SnapToGrid(newPos);
+        }
+        else if (!TransformControls.MyInstance.SnapToGridToggle.isOn)
+        {
+            /*If not locked to grid.*/
+            newPos = CustomControllers.NonSnapToGrid(newPos);
+        }
+
+        //update model position and set the y axis to 0
+        newPos.y = (transform.position.y);
+        transform.position = newPos;
+
+        MyCollisions();
+    }
+    private void LateUpdate()
+    {
+        //Stop placing the model
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CustomControllers.DestroyGameObject(gameObject);
+        }
+
+        //Rotate the model by 90deg
+        if (Input.GetMouseButtonDown(1) && GameManager.MyInstance.ActiveModel)
+        {
+            transform.RotateAround(ObjectMeshRenderer.bounds.center, Vector3.up, 90f);
+        }
+
+        if (Input.GetMouseButtonUp(0) && colliderAlert == false)
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                PlaceObject(newPos);
+            }
+        }
+        if (PlaceableObject.cost > GameManager.MyInstance.TotalMoney)
+        {
+            if (ObjectMeshRenderer.material != MaterialManager.RedMat)
+            {
+                ObjectMeshRenderer.material = MaterialManager.RedMat;
+            }
+        }
+    }
+    public void SetRampActive()
+    {
+        //initialize the Ramp stamp
+        pos = CustomControllers.NonSnapToGrid(pos);
+
+        Instantiate(gameObject, pos, Quaternion.identity, MaterialManager.ObjectWorldHolder);
+
+    }
+
+    //The model will not be able to be placed if it collides with another model in the world
+    public void MyCollisions()
+    {
+        Collider[] otherElementColliders = Physics.OverlapBox(ObjectBoxCollider.bounds.center, ObjectTransform.localScale / 2, transform.rotation, LayerMask);
+
+        if (otherElementColliders.Length <= 1)
+        {
+            ObjectMeshRenderer.material = MaterialManager.GreenMat;
+            if (colliderAlert)
+            {
+                ColliderAlert = false;
+            }
+        }      
+        else if (otherElementColliders.Length > 1)
+        {
+            ObjectMeshRenderer.material = MaterialManager.RedMat;
+            if (!colliderAlert)
+            {
+                ColliderAlert = true;
+            }
+        }
+        
+    }
+
+    //Place a new instance of the model in the world.
+    public void PlaceObject(Vector3 clickPoint)
+    {
+        if (PlaceableObject.cost <= GameManager.MyInstance.TotalMoney)
+        {
+            numOfRamps++;
+            GameObject go = Instantiate(gameObject, clickPoint, transform.rotation, MaterialManager.ObjectWorldHolder);
+            go.transform.GetChild(0).GetComponent<MeshRenderer>().material = MaterialManager.StandardMat;
+            go.GetComponent<PlaceObjectInWorld>().enabled = false;
+
+            #region Only used in editor
+#if UNITY_EDITOR
+            go.transform.name = string.Format("{0}{1}", "Ramp Container: ", numOfRamps);
+            go.transform.GetChild(0).GetComponent<Transform>().name = string.Format("{0}{1}", "Ramp: ", numOfRamps);
+#endif
+            #endregion
+
+            GameManager.MyInstance.ReduceMoney(PlaceableObject.cost);
+
+            if (!PlaceableObject.multiPlace)
+            {
+                CustomControllers.DestroyGameObject(gameObject);
+                GameManager.MyInstance.ActiveModel = null;
+            }
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+                    
+        if (PlaceableObject.placeUnderground)
+        {
+
+            if (collision.transform.gameObject.layer == 9)
+            {
+                foreach (ContactPoint contact in collision.contacts)
+                {
+                    var floorBelowObject = collision.transform.GetComponent<FloorData>();
+                    if (!GameManager.MyInstance.ActiveModel)
+                    {
+                        if (floorBelowObject.enabled)
+                        {
+                            floorBelowObject.DisableThis();
+                        }
+                    }
+                    if (GameManager.MyInstance.ActiveModel)
+                    {
+                        floorBelowObject.EnableThis();
+                    }
+                }
+            }
+            
+        }
+
+    }
+
+    #region Ramp container bounding box
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+
+    //    Matrix4x4 rotationMatrix = Matrix4x4.TRS((newPos), transform.rotation, ObjectTransform.transform.localScale);
+
+    //    Gizmos.matrix = transform.localToWorldMatrix;
+    //    Gizmos.DrawWireCube(ObjectBoxCollider.center, ObjectTransform.localScale);
+    //}
+    #endregion
+}
